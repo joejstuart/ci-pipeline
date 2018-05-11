91c7fe63386f0649f04080f80c3dd8cae8e7d390
Joe Stuart <jstuart@redhat.com>
2018-05-11 17:42:01 -0500

    using plugins.txt and init.groovy

--- config/s2i/jenkins/master/configuration/init.groovy
+++ config/s2i/jenkins/master/configuration/init.groovy
@@ -1,16 +1,24 @@
+@Grapes([
+    @Grab(group='org.yaml', module='snakeyaml', version='1.17')
+])
+
+import org.yaml.snakeyaml.Yaml
+import javaposse.jobdsl.dsl.DslScriptLoader
+import javaposse.jobdsl.plugin.JenkinsJobManagement
+import java.util.logging.Logger
+import hudson.security.*
+import jenkins.*
+import jenkins.model.*
 import java.util.logging.Logger
 import jenkins.security.s2m.*
-import jenkins.model.*;
 import com.redhat.jenkins.plugins.ci.*
 import com.redhat.jenkins.plugins.ci.messaging.*
-import hudson.security.*
 
 def logger = Logger.getLogger("")
 logger.info("Disabling CLI over remoting")
 jenkins.CLI.get().setEnabled(false);
 logger.info("Enable Slave -> Master Access Control")
 Jenkins.instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false);
-jenkins.instance.setAuthorizationStrategy(AuthorizationStrategy.Unsecured)
 Jenkins.instance.save()
 
 logger.info("Setup fedora-fedmsg Messaging Provider")
@@ -27,3 +35,25 @@ GlobalCIConfiguration.get().addMessageProvider(fedmsgDevel)
 
 logger.info("Setting Time Zone to be EST")
 System.setProperty('org.apache.commons.jelly.tags.fmt.timeZone', 'America/New_York')
+
+env = System.getenv()
+JENKINS_SETUP_YAML = env['JENKINS_SETUP_YAML'] ?: "${env['JENKINS_HOME']}/setup.yml"
+config = new Yaml().load(new File(JENKINS_SETUP_YAML).text)
+Logger logger = Logger.getLogger('seed.groovy')
+//def strategy = new GlobalMatrixAuthorizationStrategy()
+//strategy.add(hudson.model.Item.CREATE,'anonymous')
+//Jenkins.instance.setAuthorizationStrategy(strategy)
+//Jenkins.instance.save()
+
+Thread.start {
+    WORKSPACE_BASE = "${env['JENKINS_HOME']}"
+    def workspace = new File("${WORKSPACE_BASE}")
+    // workspace.mkdirs()
+    // def seedJobDsl = new File("${WORKSPACE_BASE}/seed.groovy")
+    def seedJobDsl = config.seed_jobdsl
+
+    def jobManagement = new JenkinsJobManagement(System.out, [:], workspace)
+    new DslScriptLoader(jobManagement).runScript(seedJobDsl)
+    logger.info('Created first job')
+}
+
