e7dab1bc310b7f60e73bccd88d26502799ec55c9
Joe Stuart <jstuart@redhat.com>
2018-05-11 13:22:50 -0500

    hardcode setup.yaml

--- config/s2i/jenkins/master/configuration/init.groovy.d/seed.groovy
+++ config/s2i/jenkins/master/configuration/init.groovy.d/seed.groovy
@@ -8,7 +8,7 @@ import javaposse.jobdsl.plugin.JenkinsJobManagement
 import java.util.logging.Logger
 
 env = System.getenv()
-JENKINS_SETUP_YAML = env['JENKINS_SETUP_YAML'] ?: "${env['JENKINS_HOME']}/setup.yml"
+JENKINS_SETUP_YAML = "/var/lib/jenkins/setup.yml"
 config = new Yaml().load(new File(JENKINS_SETUP_YAML).text)
 Logger logger = Logger.getLogger('seed.groovy')
 
