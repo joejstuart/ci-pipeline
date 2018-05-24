fd1d3a048f15bd9d1c6c3420078b8d6f38618eea
jstuart <jstuart@redhat.com>
2018-05-24 08:45:27 -0500

    adding run as user

--- config/Dockerfiles/elasticsearch/bin/docker-entrypoint.sh
+++ config/Dockerfiles/elasticsearch/bin/docker-entrypoint.sh
@@ -5,15 +5,16 @@ set -xe
 umask 0002
 
 run_as_other_user_if_needed() {
-    #if [[ "$(id -u)" == "0" ]]; then
-    #    # If running as root, drop to specified UID and run command
-    #    echo "running with chroot"
-    #    exec chroot --userspec=1000 / "${@}"
-    #else
+    if [[ "$(id -u)" == "0" ]]; then
+        # If running as root, drop to specified UID and run command
+        echo "running with chroot"
+        #exec chroot --userspec=1000 / "${@}"
+        exec chroot --userspec=${RUN_AS_USER} / "${@}"
+    else
         # Either we are running in Openshift with random uid and are a member of the root group
         # or with a custom --user
         exec "${@}"
-    #fi
+    fi
 }
 
 # Allow user specify custom CMD, maybe bin/elasticsearch itself
