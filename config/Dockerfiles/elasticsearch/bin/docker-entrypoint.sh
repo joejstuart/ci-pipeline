d0a54ed50c6ffcf3e06e2b22c24d7eb39e3d2ab7
jstuart <jstuart@redhat.com>
2018-05-24 08:31:19 -0500

    removing chroot

--- config/Dockerfiles/elasticsearch/bin/docker-entrypoint.sh
+++ config/Dockerfiles/elasticsearch/bin/docker-entrypoint.sh
@@ -5,15 +5,15 @@ set -xe
 umask 0002
 
 run_as_other_user_if_needed() {
-    if [[ "$(id -u)" == "0" ]]; then
-        # If running as root, drop to specified UID and run command
-        echo "running with chroot"
-        exec chroot --userspec=1000 / "${@}"
-    else
+    #if [[ "$(id -u)" == "0" ]]; then
+    #    # If running as root, drop to specified UID and run command
+    #    echo "running with chroot"
+    #    exec chroot --userspec=1000 / "${@}"
+    #else
         # Either we are running in Openshift with random uid and are a member of the root group
         # or with a custom --user
         exec "${@}"
-    fi
+    #fi
 }
 
 # Allow user specify custom CMD, maybe bin/elasticsearch itself
