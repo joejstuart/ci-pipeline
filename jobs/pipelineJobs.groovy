1e8b5d1253d2dd278c687f12357b2707b3c615d9
jstuart <jstuart@redhat.com>
2018-06-06 09:44:06 -0500

    adding pr trigger

--- jobs/pipelineJobs.groovy
+++ jobs/pipelineJobs.groovy
@@ -4,9 +4,9 @@ def Jobs = ['fedora-f26': ['gitUrl': 'https://github.com/CentOS-PaaS-SIG/upstrea
                                   'branch': 'master',
                                   'script': 'Jenkinsfile'
                                  ],
-            'fedora-trigger': ['gitUrl': 'https://github.com/CentOS-PaaS-SIG/upstream-fedora-pipeline.git',
-                               'branch': 'master',
-                               'script': 'JenkinsfileTrigger'
+            'fedora-trigger': ['gitUrl': 'https://github.com/joejstuart/upstream-fedora-pipeline.git',
+                               'branch': 'CONTRA-503',
+                               'script': 'JenkinsfilePRTrigger'
                               ]
            ]
 
