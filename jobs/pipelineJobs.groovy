f8a7802ce85cc4cc81cd479f2a89af810833c8cc
jstuart <jstuart@redhat.com>
2018-06-06 09:09:30 -0500

    changing url

--- jobs/pipelineJobs.groovy
+++ jobs/pipelineJobs.groovy
@@ -1,9 +1,9 @@
 import org.centos.dsl.pipelineJobTrigger
 
-def triggerJobs = ['fedora-f26': ['gitUrl': 'git@github.com:CentOS-PaaS-SIG/ci-pipeline.git',
-                                   'branch': 'master'
-                                  ]
-                   ]
+def triggerJobs = ['fedora-f26': ['gitUrl': 'https://github.com/CentOS-PaaS-SIG/upstream-fedora-pipeline.git',
+                                  'branch': 'master'
+                                 ]
+                  ]
 
 triggerJobs.each { jobName, jobProps ->
     pipelinejob = new pipelineJobTrigger(this, jobName, jobProps['gitUrl'], jobProps['branch'])
