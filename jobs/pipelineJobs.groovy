77996e95f998ebbb048f5d9d5319487501dd3c72
jstuart <jstuart@redhat.com>
2018-06-06 09:38:35 -0500

    adding new job

--- jobs/pipelineJobs.groovy
+++ jobs/pipelineJobs.groovy
@@ -1,12 +1,16 @@
 import org.centos.dsl.pipelineJobTrigger
 
-def triggerJobs = ['fedora-f26': ['gitUrl': 'https://github.com/CentOS-PaaS-SIG/upstream-fedora-pipeline.git',
+def Jobs = ['fedora-f26': ['gitUrl': 'https://github.com/CentOS-PaaS-SIG/upstream-fedora-pipeline.git',
                                   'branch': 'master',
                                   'script': 'Jenkinsfile'
-                                 ]
-                  ]
+                                 ],
+            'fedora-trigger': ['gitUrl': 'https://github.com/CentOS-PaaS-SIG/upstream-fedora-pipeline.git',
+                               'branch': 'master',
+                               'script': 'JenkinsfileTrigger'
+                              ]
+           ]
 
-triggerJobs.each { jobName, jobProps ->
+Jobs.each { jobName, jobProps ->
     pipelinejob = new pipelineJobTrigger(this, jobName, jobProps['gitUrl'], jobProps['branch'], jobProps['script'])
     pipelinejob.create()
 }
